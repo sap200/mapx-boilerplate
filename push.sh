5008a1113ad67fef9ecb8d08f88f0beba5112983
sapta <www.saptarsi@gmail.com>
2025-06-29 22:57:59 +0200

    boilerplate-commit

--- push.sh
+++
@@ -1,7 +0,0 @@
-echo "# mapx-boilerplate" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/sap200/mapx-boilerplate.git
-git push -u origin main
